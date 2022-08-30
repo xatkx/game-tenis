bbc875b34a440c2ab2f406cee1d41c3a69f5ed0a
Abyss <xatkx1@gmail.com>
2022-08-30 01:18:43 -0400

    primera parte rebote de pelota y coualicion en paredes 1.0

--- main.js
+++ main.js
@@ -14,40 +14,54 @@ canvas.height = pantalla.height;
 canvas.classList.add('shadow')
 
 const ctx = canvas.getContext('2d');
-var xd = 350;
+var xd = pantalla.width / 2;
 var yd = 20;
+var moviminetox = 5,moviminetoy = 5;
 
+
+var pelota = 20;
 function juego(){
-
     refresFrame(ctx,pantalla);
-    circulo(ctx,xd,yd)
+
+    circulo(ctx,pelota,xd,yd);
+
+    if(yd < pelota  || yd > pantalla.height - pelota){
+        moviminetoy = -moviminetoy;
+    }
+    if(xd < pelota || xd > pantalla.width - pelota){
+        moviminetox = -moviminetox
+    }
+    
  //console.log(yd)
- 
+
+ xd += moviminetox
+ yd += moviminetoy
 }
 
-document.addEventListener('keydown', e => {
+// document.addEventListener('keydown', e => {
 
-    let iterador = 10;
-    if(e.key == 'ArrowUp'){
-        yd -= iterador;
-    } 
     
-    if(e.key == 'ArrowDown'){
-        yd += iterador;
-    }
-    if(e.key == 'ArrowRight'){
-        xd += iterador;
-    }
-    if(e.key == 'ArrowLeft'){
-        xd -= iterador;
-    }
-})
+//     let iterador = 20,pelota = 20;
+//     if(e.key == 'ArrowUp' && yd > pelota){
+//         yd -= iterador;
+//     } 
+    
+//     if(e.key == 'ArrowDown' && yd < pantalla.height - pelota){
+//         yd += iterador;
+//     }
+//     if(e.key == 'ArrowRight' && xd < pantalla.width - pelota){
+//         xd += iterador;
+//     }
+//     if(e.key == 'ArrowLeft' && xd > pelota){
+//         xd -= iterador;
+//     }
+// })
 
-function circulo(constex,x,y){
-    let ancho = 20;
+function circulo(constex,ancho,x,y){
+    
     constex.fillStyle = 'red'
     constex.beginPath()
-    constex.arc(x,y,ancho,0, 13);
+    constex.arc(x,y,ancho,0, Math.PI *2);
     constex.fill()
     constex.closePath()
 
