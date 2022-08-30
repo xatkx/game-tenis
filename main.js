83227dc997bdde442e70bef7a29ef136c5f3e03d
Abyss <xatkx1@gmail.com>
2022-08-30 02:41:09 -0400

    barra y teclado cruz agregado falta coalicion y objetivos

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let pantalla = {
-    width:750, 
-    height:500
+    width: 750,
+    height: 500
 }
 
 const form = document.querySelector('.form');
@@ -16,58 +16,88 @@ canvas.classList.add('shadow')
 const ctx = canvas.getContext('2d');
 var xd = pantalla.width / 2;
 var yd = 20;
-var moviminetox = 5,moviminetoy = 5;
-
-
+var moviminetox = 5, moviminetoy = 5;
 var pelota = 20;
-function juego(){
-    refresFrame(ctx,pantalla);
 
-    circulo(ctx,pelota,xd,yd);
-
-    if(yd < pelota  || yd > pantalla.height - pelota){
-        moviminetoy = -moviminetoy;
-    }
-    if(xd < pelota || xd > pantalla.width - pelota){
-        moviminetox = -moviminetox
-    }
-    
- //console.log(yd)
-
- xd += moviminetox
- yd += moviminetoy
+// barra 
+let barra = {
+    width: 90,
+    height: 10,
+    position: pantalla.width / 2
 }
 
-// document.addEventListener('keydown', e => {
+var barraX = barra.position - (90 / 2);
 
-    
-//     let iterador = 20,pelota = 20;
-//     if(e.key == 'ArrowUp' && yd > pelota){
-//         yd -= iterador;
-//     } 
-    
-//     if(e.key == 'ArrowDown' && yd < pantalla.height - pelota){
-//         yd += iterador;
-//     }
-//     if(e.key == 'ArrowRight' && xd < pantalla.width - pelota){
-//         xd += iterador;
-//     }
-//     if(e.key == 'ArrowLeft' && xd > pelota){
-//         xd -= iterador;
-//     }
-// })
+var arrowLeft = false;
+var arrowRight = false;
 
-function circulo(constex,ancho,x,y){
-    
+function juego() {
+    let barraSpeed = 7;
+    refresFrame(ctx, pantalla);
+
+    circulo(ctx, pelota, xd, yd);
+
+    barraFrame(barra);
+
+    if (yd < pelota || yd > pantalla.height - pelota) {
+        moviminetoy = -moviminetoy;
+    }
+    if (xd < pelota || xd > pantalla.width - pelota) {
+        moviminetox = -moviminetox
+    }
+
+    //---------------
+
+    if(arrowRight && barraX < pantalla.width - barra.width){
+        barraX += barraSpeed;
+    }
+    if(arrowLeft && barraX > 0){
+       barraX -= barraSpeed;
+    }
+
+    //console.log(yd)
+    xd += moviminetox
+    yd += moviminetoy
+}
+
+document.addEventListener('keyup', e => {
+    if(e.key == 'ArrowRight'){
+        arrowRight = false
+    }
+    if(e.key == 'ArrowLeft'){
+       arrowLeft = false
+    }
+})
+document.addEventListener('keydown', e => {
+    if(e.key == 'ArrowRight' && xd < pantalla.width - pelota){
+        arrowRight = true
+    }
+    if(e.key == 'ArrowLeft' && xd > pelota){
+       arrowLeft = true
+    }
+})
+
+function barraFrame(json) {
+    const altura = 460;
+    ctx.fillStyle = 'red'
+    ctx.beginPath()
+    ctx.rect(barraX, altura, json.width, json.height)
+    ctx.fill();
+    ctx.closePath();
+
+}
+
+function circulo(constex, ancho, x, y) {
+
     constex.fillStyle = 'red'
     constex.beginPath()
-    constex.arc(x,y,ancho,0, Math.PI *2);
+    constex.arc(x, y, ancho, 0, Math.PI * 2);
     constex.fill()
     constex.closePath()
 
 }
-function refresFrame(contex,pa){
+function refresFrame(contex, pa) {
 
-    contex.clearRect(0,0,pa.width,pa.height)
+    contex.clearRect(0, 0, pa.width, pa.height)
 }
-setInterval(juego,10);
+setInterval(juego, 15);
