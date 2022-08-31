e8e0b55430467c3c04ac76d17044acd29abb3d18
Abyss <xatkx1@gmail.com>
2022-08-31 19:05:11 -0400

    colicion con bloques

--- main.js
+++ main.js
@@ -3,21 +3,49 @@ let pantalla = {
     height: 500
 }
 
+// ladrillos
+
+let enemigos = {
+    width: 75,
+    height:30,
+    row: 3,
+    column: 8,
+    gapbetween: 10,
+    gaptop: 20,
+    gapleft: 40
+}
+
+var ladrillos = []
+for (let c = 0; c < enemigos.column; c++) {
+
+    ladrillos[c]= [];
+
+    for (let r = 0; r < enemigos.row; r++) {
+
+        ladrillos[c][r] = {x:0, y:0, stats: true}
+        
+    }
+}
+
+//========
 const form = document.querySelector('.form');
 form.classList.add('shadow')
 form.style.display = 'none'
 
-
+//canvas
 const canvas = document.querySelector('#juego');
 canvas.width = pantalla.width;
 canvas.height = pantalla.height;
 canvas.classList.add('shadow')
 
 const ctx = canvas.getContext('2d');
+//============
+// ball
 var ball_X = pantalla.width / 2;
 var ball_Y = 400;
-var moviminetox = 5, moviminetoy = -5;
+var moviminetox = aleat(-5,5), moviminetoy = -5;
 var pelota = 20;
+//============
 
 // barra 
 let barra = {
@@ -28,13 +56,14 @@ let barra = {
 var barraX = barra.position - (90 / 2);
 var arrowLeft = false;
 var arrowRight = false;
-
+//============
 function juego() {
     let barraSpeed = 7;
     refresFrame(ctx, pantalla);
 
     circulo(ctx, pelota, ball_X, ball_Y);
-
+    brickFrame(enemigos)
+    colicionBricks()
     barraFrame(barra);
 
     if (ball_Y < pelota ) {
@@ -121,14 +150,65 @@ function circulo(constex, ancho, x, y) {
     constex.closePath()
 
 }
-function refresFrame(contex, pa) {
 
-    contex.clearRect(0, 0, pa.width, pa.height)
+function bricks(json,ladrillo){
+    ctx.fillStyle = 'blue'
+    ctx.beginPath();
+    ctx.rect(ladrillo.x,ladrillo.y,json.width,json.height);
+    ctx.fill();
+    ctx.closePath();
+
 }
 
+
+
+function brickFrame(json){
+    
+    for( let c = 0; c < json.column ; c++)
+    {
+        
+        for(let r = 0 ;r < json.row; r++)
+        {
+            
+
+            ladrillos[c][r].x = (c*(json.width + json.gapbetween))+ json.gapleft
+            ladrillos[c][r].y = (r*(json.width + json.gapbetween))+ json.gaptop
+            if(ladrillos[c][r].stats){
+
+                bricks(json,ladrillos[c][r])
+            }
+        }     
+    }
+    
+}
+
+function colicionBricks(){
+    let position
+    for(let c = 0;c < enemigos.column ; c++){
+        for(let r = 0; r < enemigos.row; r++){
+           position = ladrillos[c][r]
+
+           if(ball_X > position.x && ball_X < position.x + enemigos.width)
+           {
+                if(ball_Y > position.y && ball_Y < position.y + enemigos.height){
+                    
+                    console.log(ladrillos[c][r].stats)
+                    if(ladrillos[c][r].stats){
+                        ladrillos[c][r].stats = false
+                        moviminetoy = -moviminetoy
+                    }
+                }
+           }
+        }
+    }
+}
+
+function refresFrame(contex, pa) {
+    contex.clearRect(0, 0, pa.width, pa.height)
+}
 function aleat (max,min){
     return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
 
-//setInterval(juego, 10);
+setInterval(juego, 10);
